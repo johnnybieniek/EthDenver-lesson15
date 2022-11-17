850ab9d74595c1f7d923e8788452eb92f2cbffda
Johnny Bieniek <jjbieniek@gmail.com>
2022-11-16 19:31:14 -0500

    code after class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ethers } from 'ethers';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'my-first-app';
+
+  someText = 'This is some text';
+  counter = 0;
+
+  clickMe() {
+    this.counter++;
+  }
 }
