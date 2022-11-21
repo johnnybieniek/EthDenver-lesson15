d1b986228e25ab349a6740f0e84513069d099240
Johnny Bieniek <jjbieniek@gmail.com>
2022-11-21 11:41:50 -0500

    added functionality to get and display user's balance and voting power

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { ethers } from 'ethers';
 import { HttpClient } from '@angular/common/http';
 
+const getProposalsRequest = 'http://localhost:3000/get-proposals';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,16 +17,21 @@ export class AppComponent {
   defaultState: boolean | undefined;
   proposalList: string[];
   proposalVoteCount: number[];
+  userBalance: number;
+  userVotingPower: number;
+  mintResult: string | undefined;
 
   constructor(private http: HttpClient) {
     this.seeProposals = false;
     this.defaultState = true;
     this.proposalList = [];
     this.proposalVoteCount = [];
+    this.userBalance = 0;
+    this.userVotingPower = 0;
 
     if (this.proposalList.length == 0) {
       this.http
-        .get<string[]>('http://localhost:3000/get-proposals')
+        .get<string[]>(getProposalsRequest)
         .subscribe((proposalArray) => {
           console.log(proposalArray);
           this.proposalList = proposalArray;
@@ -43,6 +50,24 @@ export class AppComponent {
     this.defaultState = false;
     this.provider = ethers.providers.getDefaultProvider('goerli');
     this.randomWallet = ethers.Wallet.createRandom().connect(this.provider);
+
+    const balanceRequest = 'http://localhost:3000/user-balance/'.concat(
+      this.randomWallet.address
+    );
+
+    this.http.get<number>(balanceRequest).subscribe((balance) => {
+      console.log(`balance: ${balance}`);
+      this.userBalance = balance;
+    });
+
+    const votingPowerRequest =
+      'http://localhost:3000/user-voting-power/'.concat(
+        this.randomWallet.address
+      );
+    this.http.get<number>(votingPowerRequest).subscribe((votingPower) => {
+      console.log(`voting power: ${votingPower}`);
+      this.userVotingPower = votingPower;
+    });
   }
 
   showProposals() {
